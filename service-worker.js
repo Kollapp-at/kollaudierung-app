99aeb767d739f5d88935f7ea5778396294c3d4c2
Kollapp-at <dominichoeflet@gmail.com>
2026-08-30 01:52:48 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "kollaudierung-v6.96-shell-1";
-const RUNTIME = "kollaudierung-v6.96-runtime-1";
+const CACHE = "kollaudierung-v6.97-shell-1";
+const RUNTIME = "kollaudierung-v6.97-runtime-1";
 
 const SHELL = [
   "./",
